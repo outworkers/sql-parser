6a21a91c228619e7cead3f8162a1900263af48ab
Flavian Alexandru <flavian@outworkers.com>
2017-12-31 05:22:52 +0000

    Adding a test runner to automatically produce and checking dos.

---
+++ build/run_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+function run_test_suite {
+    if [ "${TRAVIS_SCALA_VERSION}" == "${TARGET_SCALA_VERSION}" ];
+    then
+        echo "Running tests with coverage and report submission"
+        sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate coveralls
+        testExitCode=$?
+
+        if [ ${testExitCode} == "0" ];
+        then
+            echo "Running tut compilation"
+            sbt ++$TRAVIS_SCALA_VERSION "project readme" "tut"
+            exit $?
+        else
+            echo "Unable to run tut compilation, test suite failed"
+            exit ${testExitCode}
+        fi
+    else
+        echo "Running tests without attempting to submit coverage reports or run tut"
+        sbt "plz $TRAVIS_SCALA_VERSION test"
+        exit $?
+    fi
+}
+
+run_test_suite
